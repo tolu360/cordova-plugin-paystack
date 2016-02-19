faaba3c064b73512f5c5942df12174933adc2f87
Tolu Olowu <o.tolulope@gmail.com>
2016-02-19 20:35:11 +0100

    First Commit - v1.0.1

---
+++ src/android/PaystackPlugin.java
@@ -0,0 +1,184 @@
+package com.arttitude360.cordova.sdk;
+
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.net.Uri;
+
+import co.paystack.android.Paystack;
+import co.paystack.android.PaystackSdk;
+import co.paystack.android.model.Card;
+import co.paystack.android.model.Token;
+
+public class PaystackPlugin extends CordovaPlugin {
+
+	protected Token token;
+	protected Card card;
+
+	/**
+     * Cordova callback context
+     */
+    protected CallbackContext context;
+
+	/**
+     * Sets the context of the Command. This can then be used to do things like
+     * get file paths associated with the Activity.
+     *
+     * @param cordova The context of the main Activity.
+     * @param webView The CordovaWebView Cordova is running in.
+     */
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        
+        //initialize sdk
+    	PaystackSdk.initialize(this.cordova.getContext());
+    }
+
+	@Override
+    public boolean execute(String action, JSONArray args,
+                           CallbackContext callbackContext) throws JSONException {
+        boolean result;
+        try {
+            if(action.equals("getToken")) {
+                context = callbackContext;
+                getToken(args);
+                result = true;
+            } else {
+                handleError("Invalid action");
+                result = false;
+            }
+        } catch(Exception e ) {
+            handleError(e.getMessage());
+            result = false;
+        }
+        return result;
+    }
+
+    protected void handleError(String errorMsg){
+        try {
+            Log.e(TAG, errorMsg);
+            JSONObject error = new JSONObject();
+            error.put("error", errorMsg);
+            context.error(error);
+        } catch (JSONException e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    protected void handleSuccess(String msg){
+        try {
+            Log.i(TAG, msg);
+            JSONObject success = new JSONObject();
+            success.put("message", msg);
+            context.success(success);
+        } catch (JSONException e) {
+            handleError(e.getMessage(), ERROR_EXCEPTION);
+        }
+    }
+
+    private void getToken(JSONArray args) {
+    	//build a card
+		// Card card = new Card.Builder(cardNumber, expiryMonth, expiryYear, cvc).build();
+		// cardNumber: String
+		// expiryMonth: Integer 1-12
+		// expiryYear: Integer 2016
+		// cvc: String 234
+		
+
+		//check card validity
+        validateCard(args);
+		
+		if (card.isValid()) {
+			createToken(card);
+		}
+    }
+
+    protected void validateCard(JSONArray args) {
+		String cardNum = args.getString(0).trim();
+
+		if (isEmpty(cardNum)) {
+			handleError("Empty card number");
+			return;
+		}
+
+		//build card object with ONLY the number, update the other fields later
+		card = new Card.Builder(cardNum, 0, 0, "").build();
+
+		if (!card.validNumber()) {
+			handleError("Invalid card number");
+			return;
+		}
+
+		//validate cvc
+		String cvc = args.getString(3).trim();
+		if (isEmpty(cvc)) {
+			handleError("Empty cvc");
+			return;
+		}
+		
+		//update the cvc field of the card
+		card.setCvc(cvc);
+
+		//check that it's valid
+		if (!card.validCVC()) {
+			handleError("Invalid cvc");
+			return;
+		}
+
+		//validate expiry month;
+		Integer expiryMonth = args.getInt(1);
+		
+		if (expiryMonth < 1) {
+			handleError("Invalid month");
+			return;
+		}
+
+		//update the expiryMonth field of the card
+		card.setExpiryMonth(expiryMonth);
+
+		//validate expiry year;
+		Integer expiryYear = args.getInt(2)
+		
+		if (expiryYear < 1) {
+			handleError("Invalid year");
+			return;
+		}
+
+		//update the expiryYear field of the card
+		card.setExpiryYear(expiryYear);
+
+		//validate expiry
+		if (!card.validExpiryDate()) {
+			handleError("Invalid expiry");
+		}
+    }
+
+	private void createToken(Card card) {
+		//then create token using PaystackSdk class
+		PaystackSdk.createToken(card, new Paystack.TokenCallback() {
+			@Override
+			public void onCreate(Token token) {
+				//here you retrieve the token, and send to your server for charging.
+				handleSuccess(token);
+			
+			}
+
+			@Override
+			public void onError(Exception error) {
+				handleError(error.getMessage());
+			}
+		});
+	}
+
+	private boolean isEmpty(String s) {
+		return s == null || s.length() < 1;
+	}
+}
---
+++ www/cdv-plugin-paystack-sdk.js
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2016 Arttitude360. All rights reserved.
+//
+
+/**
+ * This class exposes the Paystack Android SDK functionality to javascript.
+ *
+ * @constructor
+ */
+function PaystackPlugin() {}
+
+/**
+ *
+ * @param [Function} successCallback - callback to be invoked on successfully acquiring a token.
+ * A single object argument will be passed which has a single key: "message" is a string containing the returned token.
+ * @param {Function} errorCallback - callback to be invoked on failure to acquire a valid token.
+ * A single object argument will be passed which has a single key: "error" is a string containing a description of the error.
+ * @param {Array} The card details in the order - cardNumber, expiryMonth, expiryYear, cvc.
+ */
+PaystackPlugin.prototype.getToken = function(successCallback, errorCallback, cardNumber, expiryMonth, expiryYear, cvc) {
+	return cordova.exec(successCallback,
+		errorCallback,
+		'PayPalMobile',
+		'getToken',
+		[cardNumber, expiryMonth, expiryYear, cvc]);
+};
+
+module.exports = new PaystackPlugin();
